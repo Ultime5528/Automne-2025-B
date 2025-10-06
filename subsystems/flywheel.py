53318919418c2c7300f5176a3a2ec10ee0c87f44
- <aaa@bbb.com>
2025-10-06 17:52:02 -0400

    Flywheel code

--- subsystems/flywheel.py
+++ subsystems/flywheel.py
@@ -1,13 +1,24 @@
-from enum import Enum, auto
+
+from wpilib import VictorSP
 
 import ports
 from ultime.autoproperty import autoproperty
 from ultime.subsystem import Subsystem
 
-class Flywheel(Subsystem):
-    class State(Enum):
-        Unknown = auto()
-        Moving = auto()
-        Loading = auto()
 
-    speed = autoproperty(0.2)
+class Flywheel(Subsystem):
+
+    FWspeed = autoproperty(0.2)
+
+    def  __init__(self):
+
+        super().__init__()
+
+        self.motor = VictorSP(ports.SIM.flywheel_motor)
+
+
+    def startFw(self):
+        self._motor.setspeed(self.FWspeed)
+
+    def stopFw(self):
+        self._motor.stopMotor()
