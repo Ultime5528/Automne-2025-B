8be20db07dc0f7af584c29c4c00b3f564c9bb43b
Charles R. <4201224@keranna.qc.ca>
2025-10-06 16:30:29 -0400

    Removed some more code

--- subsystems/flywheel.py
+++ subsystems/flywheel.py
@@ -5,9 +5,4 @@ from ultime.autoproperty import autoproperty
 from ultime.subsystem import Subsystem
 
 class Flywheel(Subsystem):
-    class State(Enum):
-        Unknown = auto()
-        Moving = auto()
-        Loading = auto()
-
-    speed = autoproperty(0.2)
+    pass
